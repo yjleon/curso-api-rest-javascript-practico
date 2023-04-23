5423e3eba3db084a093cb4a7daddffc7c2ff7f8f
yjleon <yjleona7@gmail.com>
2023-04-23 14:52:21 -0500

    llamar generos desde api

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 async function getTrendingMoviesPreview(){
-    const res = await fetch ('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY); 
+    const res = await fetch (
+        'https://api.themoviedb.org/3/trending/movie/day?api_key='
+         + API_KEY
+         ); 
     const data = await res.json(); 
 
     const movies = data.results;
-    console.log({data, movies}); 
 
     movies.forEach(movie => {
         const trendingPreviewMoviesContainer = document.querySelector 
-        ('#trendingPreview .trendingPreview-movieList')
+        ('#trendingPreview .trendingPreview-movieList');
 
         const movieContainer = document.createElement('div'); 
         movieContainer.classList.add('movie-container'); 
@@ -26,3 +28,35 @@ async function getTrendingMoviesPreview(){
 }
 
 getTrendingMoviesPreview(); 
+
+async function getCategoriesPreview(){
+
+    const res = await fetch (
+        'https://api.themoviedb.org/3/genre/movie/list?api_key='
+        + API_KEY
+        );
+    const data = await res.json(); 
+
+    const categories = data.genres; 
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector(
+            '#categoriesPreview .categoriesPreview-list'); 
+        
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');  
+
+        const categoryTitle = document.createElement('h3'); 
+        categoryTitle.classList.add('category-title'); 
+            categoryTitle.setAttribute('id', 'id' + category.id);
+        const categoryTitleText = document.createTextNode (
+            category.name);
+
+        categoryTitle.appendChild(categoryTitleText); 
+        categoryContainer.appendChild(categoryTitle); 
+        previewCategoriesContainer.appendChild(categoryContainer); 
+
+        
+    });   
+}
+
+getCategoriesPreview(); 
